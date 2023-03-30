e30b503c667c44a30147324f8f9dde2411c30063
pedromarcal12 <henrique.pedro.marcal@gmail.com>
2023-03-30 12:04:25 +0100

    Changing IDE to Eclipse
    Last touches before getting started with the threads

--- CaTest/src/main/java/com/mycompany/catest/CaTest.java
+++ CaTest/src/main/java/com/mycompany/catest/CaTest.java
@@ -5,8 +5,11 @@
 package com.mycompany.catest;
 
 
-import java.io.*;  
-import java.util.Scanner;  
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner; 
+
 /**
  *
  * @author 35389
@@ -14,16 +17,37 @@ import java.util.Scanner;
 public class CaTest {
 
     public static void main(String[] args) throws Exception {
-        
+    	
+     List<Double> data = new ArrayList<>();   
+     
+    	
         //Scanner method for reading the file
-        Scanner sc = new Scanner(new File("C:\\Users\\35389\\Downloads\\data.csv"));
+        Scanner sc = new Scanner(new File("C:\\Users\\henri\\Downloads\\data.csv"));
+        
         //Splitting value inside file using ,
         sc.useDelimiter(",");
+        
         //While loop to read file until last line
-        while (sc.hasNext())  {
-            System.out.println(sc.next());
+        while (sc.hasNextLine())  {
+        //Saving data into an array
+        	String value = sc.nextLine();
+        	
+        //Split values into ,	
+        	String [] splitNum = value.split(",");
+        	
+        //for loop to add value into array	
+        for(String number : splitNum) {
+        data.add(Double.valueOf(number));	
+        	
+        }
+        	System.out.println(data);
         }
         //Closing scanner
             sc.close();
+            
+        //Saving all info in one array in order to do methods    
+            double[] finalVal = data.stream().mapToDouble(Double::doubleValue).toArray();        
     }
+    
+   
 }
