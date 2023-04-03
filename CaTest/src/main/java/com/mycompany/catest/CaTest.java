2af1c857af0d81e3c9cc029d09108bfc2b237531
pedromarcal12 <henrique.pedro.marcal@gmail.com>
2023-04-03 15:53:49 +0100

    update main.java

--- CaTest/src/main/java/com/mycompany/catest/CaTest.java
+++ CaTest/src/main/java/com/mycompany/catest/CaTest.java
@@ -16,29 +16,39 @@ public class CaTest{
     	try (Scanner scanner = new Scanner(System.in)) {
 			int num;
 			
-			System.out.println("\nChoose the task");
-			System.out.println("1 - Standard Deviation");
-			System.out.println("2 - Result of the matrizes");
-			System.out.println("3 - MergeSort");
-			System.out.println("4 - All results in multithread\n");
-			System.out.println(" ");
+			System.out.println("Type down :");
+			System.out.println("1 - Standard Deviation \n" + "2 - Result of the matrizes \n" + 
+			"3 - MergeSort \n" + "4 - All results in multithread \n" + "5 - All results in multithread with sleep thread implementation\n ");
+			
 			num = scanner.nextInt();
 			
 			
 		  if (num == 1) {
 			StandardDeviation thread1 = new StandardDeviation();
 			System.out.println(thread1);
+			
 		} if (num == 2) {
 			MergeSort thread2 = new MergeSort(null, 0, 0, 0);
 			System.out.println(thread2);
+			
 		} if (num == 3) {
 			MatrizResult thread3 = new MatrizResult();
 			System.out.println(thread3);
+			
 		} if (num == 4) {
-			  StandardDeviation thread1 = new StandardDeviation();	
-			  MergeSort thread2 = new MergeSort(null, 0, 0, 0);
-			  MatrizResult thread3 = new MatrizResult();
-		} else if (num >= 5) {
+			  new StandardDeviation();	
+			  new MergeSort(null, 0, 0, 0);
+			  new MatrizResult();
+			  
+		} if (num == 5) {
+			  new StandardDeviation();
+			  StandardDeviation.sleep(500);
+			  new MergeSort(null, 0, 0, 0);
+			  MergeSort.sleep(5000);
+			  new MatrizResult();
+			  MatrizResult.sleep(4000);
+			  
+		} else if (num >= 6) {
 			System.out.println("Wrong input!");
 		}
    
